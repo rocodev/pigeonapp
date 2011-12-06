5d5db294e7641a2385a6c7482434877eae294734
xdite <xdite@techbang.com.tw>
2011-12-06 18:17:33 +0800

    inistal project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koupon::Application.config.secret_token = '9fad404d38f1392a90e570d846aa3baebab8e67bdab9be91f9f5e690b6501cf74cdd0038424eed28c6dba5edde7e1762f264a40612ffd047a338f56c6005a110'
