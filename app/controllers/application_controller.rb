b63ba68edef5189a95d7a102404224ca33626100
xdite <xuite.joke@gmail.com>
2012-01-19 16:51:19 +0800

    login should back to subdomain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def after_sign_in_path_for(resource)
+    # This should work, but session is lost. See https://github.com/plataformatec/devise/issues/1357
+    # return_to = session[:return_to]
+    # session[:return_to] = nil
+    return_to = request.env['omniauth.origin']
+    stored_location_for(resource) || return_to || root_path  
+  end
+  
 end
