419afa37263d01288a68db187c02e2879d9602a3
xdite <xdite@rocodev.com>
2012-03-05 16:52:44 +0800

    add temp hack to notice_message

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,16 @@ module ApplicationHelper
       title = SITE_NAME
     end
   end
+  
+  # temp hack
+  def notice_message
+    flash_messages = []
+    flash.each do |type, message|
+      type = :success if type == :notice
+      text = content_tag(:div, link_to("x", "#", :class => "close", "data-dismiss" => "alert") + message, :class => "alert fade in alert-#{type}")
+      flash_messages << text if message
+    end
+    flash_messages.join("\n").html_safe
+  end
+  
 end
