cc54de32bf41c9ac3cb046c908be9725337e93ed
xdite <xdite@rocodev.com>
2012-03-20 13:48:35 +0800

    enable asset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,17 +10,20 @@ DeliverIO::Application.configure do
   config.action_controller.perform_caching = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
-  config.assets.compress = true
+  
+  #config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true
+  #config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
+  config.assets.enabled = true
+  
   # Defaults to Rails.root.join("public/assets")
   # config.assets.manifest = YOUR_PATH
 
@@ -47,8 +50,8 @@ DeliverIO::Application.configure do
   # config.assets.precompile += %w( search.js )
   # 
   # 
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+  #config.assets.js_compressor  = :uglifier
+  #config.assets.css_compressor = :scss
   config.assets.precompile = ["application.css", "application.js", "bg.png"]
 
   # Disable delivery errors, bad email addresses will be ignored
